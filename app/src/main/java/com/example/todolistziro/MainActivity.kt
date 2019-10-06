33fc564b87b63df2d5679bcf4ccb90cb26a3d13e
Mohamed Ebrahim <ebrahimm131@gmail.com>
2019-10-07 01:06:58 +0200

    Add Floating Button

--- app/src/main/java/com/example/todolistziro/MainActivity.kt
+++ app/src/main/java/com/example/todolistziro/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.todolistziro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonAddNote = findViewById(R.id.button_add_note)
+        buttonAddNote.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, AddNoteActivity::class.java)
+            startActivityForResult(intent, ADD_NOTE_REQUEST)
+        })
+
 
 
     }
